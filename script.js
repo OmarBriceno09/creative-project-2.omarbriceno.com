86ed6b189db7c54ae177c14a38a7a7a5b4cf7c87
OmarBriceno09 <ec2-user@ip-172-31-39-13.us-east-2.compute.internal>
2019-10-11 19:20:00 +0000

    new sites, will work on updating menu tab after content gets done

--- script.js
+++ script.js
@@ -8,3 +8,37 @@ const url = "https://www.potterapi.com/v1/sortingHat?q=" + key;
     }).then(function(json) {	
     	console.log(json);
 	});
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("data-include");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("data-include");
+          includeHTML();
+          // document.getElementById("home").classList.add('active');
+          // document.getElementById("menu").classList.remove('active');
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+
+//changing selected menu items
+//document.getElementById("weatherSubmit").addEventListener("click", function(event) {
+//});
