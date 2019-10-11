3d8b378213406540ae27e81fcc07c71ca802215d
Davin Parish <davinparish@gmail.com>
2019-10-11 15:06:28 +0000

    added api key

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*global fetch*/
-
+// $2a$10$9Q3FcsJi06VL856epPTxluO.qJoICCBGIyCKUWUGVLus6YMk7sV3G
 const url = "http://shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]";
   fetch(url)
     .then(function(response) {
